99af1df4358bcf0e2a0a222e4bbfd1899a00aed1
AlejandroArroyo <alejandroarroyogil@gmail.com>
2025-08-01 09:33:37 +0200

    Update bot_busqueda_2.py

--- bot_busqueda_2.py
+++ bot_busqueda_2.py
@@ -27,7 +27,7 @@ def buscar_productos():
 
     # Configurar Chrome con opciones antidetención
     options = webdriver.ChromeOptions()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--1850,1200")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
